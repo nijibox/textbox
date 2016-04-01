630086e4c5c04d6c1f70caa7da0e961e21e7ca65
attakei <attakei@gmail.com>
2016-04-02 01:48:17 +0900

    管理者権限付与コマンドを申請

---
+++ app/Console/Commands/AdminSet.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Inspiring;
+use App\User;
+
+
+class AdminSet extends Command
+{
+    /**
+     * 管理者権限付与コマンド
+     *
+     * @var string
+     */
+    protected $signature = 'admin:set {email}';
+
+    /**
+     * 説明文
+     *
+     * @var string
+     */
+    protected $description = '指定したメールアドレスのユーザーに管理者権限を付与する';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // TODO: テストコード無し
+        $userEmail = $this->argument('email');
+        $user =  User::where('email', $userEmail)->first();
+        if ( is_null($user) ) {
+            $this->error('指定されたメールアドレスは見つかりませんでした');
+            return false;
+        }
+        $user->is_admin = true;
+        $user->save();
+        return true;
+    }
+}
