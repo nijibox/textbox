1bbe71070d74e4e64108d19042e738d132b435ef
attakei <attakei@gmail.com>
2016-04-02 14:09:00 +0900

    テストコードの外側だけ追加

---
+++ tests/commands/AdminSetTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class AdminSetTest extends \TestCase
+{
+    /**
+     * 存在しないメールアドレすを指定した場合は、エラーメッセージが出ること
+     *
+     * @return void
+     */
+    public function testEmailIsNotExists()
+    {
+    }
+
+    /**
+     * 存在するメールアドレスが指定された場合は、管理者権限が付与されていること
+     *
+     * @return void
+     */
+    public function testEmailIsExists()
+    {
+    }
+}
