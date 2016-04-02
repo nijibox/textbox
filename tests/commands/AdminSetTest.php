dc02f40ab56e8bb41ba7f6da93ef4cc90e0e1c71
attakei <attakei@gmail.com>
2016-04-02 14:36:21 +0900

    テストコードの外側だけ追加（ギリギリまでやりたいことを記述）

--- tests/commands/AdminSetTest.php
+++ tests/commands/AdminSetTest.php
@@ -1,7 +1,28 @@
 <?php
 
+use Symfony\Component\Console\Tester\CommandTester;
+use Illuminate\Support\Facades\DB;
+use App\User;
+use App\Console\Commands\AdminSet;
+
+
 class AdminSetTest extends \TestCase
 {
+    protected function setTestData()
+    {
+        DB::transaction(function ()
+        {
+            User::create(['name'=>'test_user_1', 'email' => 'user1@example.com', 'password' => '']);
+        });
+
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->setTestData();
+    }
+
     /**
      * 存在しないメールアドレすを指定した場合は、エラーメッセージが出ること
      *
@@ -9,6 +30,10 @@ class AdminSetTest extends \TestCase
      */
     public function testEmailIsNotExists()
     {
+        $tester = new CommandTester(new AdminSet);
+        // 引数に'not_found@example.com'を指定してコマンド実行
+        $user = User::find(1)->first();
+        $this->assertEquals($user->is_admin, '0');
     }
 
     /**
@@ -18,5 +43,12 @@ class AdminSetTest extends \TestCase
      */
     public function testEmailIsExists()
     {
+        $user = User::find(1)->first();
+        $this->assertEquals($user->is_admin, '0');
+        $tester = new CommandTester(new AdminSet);
+        // 引数に'user1@example.com'を指定してコマンド実行
+        $user = User::find(1)->first();
+        // TODO: テスト実行できるようになったらコメントから復帰
+        // $this->assertEquals($user->is_admin, '1');
     }
 }
