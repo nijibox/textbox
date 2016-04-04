bb81db66fd356b0939959dfd0a162ec347cb8031
attakei <attakei@gmail.com>
2016-04-04 19:20:20 +0900

    管理者権限持ち以外アクセスできなくするミドルウェアを用意

---
+++ app/Http/Middleware/AdminOnly.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+
+class AdminOnly
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ( !Auth::user()->is_admin ) {
+            return response('Unauthorized.', 401);
+        }
+
+        return $next($request);
+    }
+}
