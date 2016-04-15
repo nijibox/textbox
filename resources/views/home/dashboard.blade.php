2e20fe363402dbb9ab1e19c57186c866be5d916f
attakei <attakei@gmail.com>
2016-04-15 23:38:35 +0900

    サイドバーのタグ一覧をスニペット化する

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -25,11 +25,5 @@
 @endsection
 
 @section('content.sub')
-<h3>タグ</h3>
-<ul>
-    @foreach($tagSummary as $tag)
-    <li><a href="{{ route('list_by_tag', $tag->body) }}">{{ $tag->body }} [{{ $tag->count }}]</a></li>
-    @endforeach
-</ul>
-<p style="text-align: right;"><a href="{{ route('get_tags_list') }}">...もっと見る</a></p>
+@include('snippets.tags_least', ['latestTags' => $tagSummary])
 @endsection
