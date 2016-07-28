7a66af820c1091ad24494dd3b2cc096699c33914
attakei <attakei@gmail.com>
2016-07-28 12:25:24 +0900

    Refactor:

--- database/migrations/2016_07_27_145528_create_attachments_table.php
+++ database/migrations/2016_07_27_145528_create_attachments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAttachmentTable extends Migration
+class CreateAttachmentsTable extends Migration
 {
     /**
      * Run the migrations.
