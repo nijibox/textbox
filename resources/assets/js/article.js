4e4b62e65a00f7371d4c67c8a8b4263681384dd7
attakei <attakei@gmail.com>
2016-10-06 18:45:55 +0900

    Use markdown-it and highlight.js

---
+++ resources/assets/js/article.js
@@ -0,0 +1,22 @@
+var hljs = require('highlight.js'); // https://highlightjs.org/
+
+
+// Actual default values
+var md = require('markdown-it')({
+    html: true,
+    xhtmlOut: false,
+    breaks: true,
+    langPrefix: 'language-',
+    linkify: true,
+    typographer:  false,
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return '<pre class="hljs"><code>' +
+                    hljs.highlight(lang, str, true).value +
+                    '</code></pre>';
+            } catch (__) {}
+        }
+        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    }
+});
